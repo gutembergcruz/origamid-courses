40726ada79b58edb1c25d0a99732fbf6900decdd
Guteberg Cruz <gute.tec@gmail.com>
2020-07-06 02:14:36 -0300

    Variaveis

--- JavaScript Completo ES6/Variaveis/script.js
+++ JavaScript Completo ES6/Variaveis/script.js
@@ -1,1 +1,33 @@
+var nome = "Gutemberg";
+var idade = 24;
+var possuiFaculdade = false;
 
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Cruz",
+  cidade = "Manaus";
+
+console.log(nome, sobrenome, cidade);
+
+var precoAplicativo;
+
+console.log(precoAplicativo);
+
+//console.log(aindaNaoDefine);
+
+console.log(comida);
+
+var comida = "Nuggts";
+
+console.log(comida);
+
+var time = "Pain";
+time = "SKT";
+
+console.log(time);
