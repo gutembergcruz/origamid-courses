91b64c2a1df19e4c723b99dcd5d5f9a8b8290cf6
Guteberg Cruz <gute.tec@gmail.com>
2020-07-06 21:13:41 -0300

    Tipos-de-dados

---
+++ JavaScript Completo ES6/Tipos de dados/script.js
@@ -0,0 +1,24 @@
+var nome = "Gutemberg"; //String
+var idade = 28; //Number
+var possuiFaculdade = false; //Bolean
+var time; //Undefined
+var comida = null; //Null
+var simbolo = Symbol(); //Symbol
+var novoObjeto = {}; //Object
+
+console.log(typeof time);
+
+var sobrenome = "Cruz";
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = "Romário fez " + gols + " gols";
+console.log(frase);
+
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+var frase2 = `Romário fez ${gols * 8} gols`;
+console.log(frase2);
