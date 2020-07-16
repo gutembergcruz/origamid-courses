16a313ba35ff3eb2baa9a54c750e27ffe7fafc5c
Guteberg Cruz <gute.tec@gmail.com>
2020-07-16 19:46:30 -0300

    Funções-1

---
+++ JavaScript Completo ES6/Funções/script.js
@@ -0,0 +1,33 @@
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+console.log(areaQuadrado(2));
+
+function pi() {
+  return 3.14;
+}
+
+var total = 5 * pi();
+console.log(total);
+
+function imc(peso, altura) {
+  var imc = peso / (altura * altura);
+  return imc;
+}
+
+console.log(imc(47, 1.6));
+
+function corFavorita(cor) {
+  if (cor === "azul") {
+    return "Eu gosto do céu";
+  } else if (cor === "verde") {
+    return "Eu gosto de mato";
+  } else {
+    return "Eu nao gosto de cores";
+  }
+}
+
+addEventListener("click", function () {
+  console.log("oi");
+});
