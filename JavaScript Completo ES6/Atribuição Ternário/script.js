0bbb8b63a2949a7521c505fc479368490bfac145
Guteberg Cruz <gute.tec@gmail.com>
2020-07-31 00:45:18 -0300

    Atribuição Ternário

---
+++ JavaScript Completo ES6/Atribuição Ternário/script.js
@@ -0,0 +1,18 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+
+scroll += 500;
+console.log(scroll);
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = false;
+var darCredito;
+
+if (possuiCarro && possuiCasa) {
+  darCredito = true;
+} else {
+  darCredito = false;
+}
